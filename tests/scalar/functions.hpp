c52ce01dc49d90044b1daffe6f600f716b5a7e55
betanalpha <betanalpha@gmail.com>
2014-08-06 23:21:21 +0100

    One-Executable Per Test Organization

--- tests/scalar/functions.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef nomad__tests__scalar__functions_hpp
-#define nomad__tests__scalar__functions_hpp
-
-#include <tests/scalar/functions/smooth_functions/acos.hpp>
-
-#endif
--- tests/scalar/run_scalar_function_tests.cpp
+++ tests/scalar/run_scalar_function_tests.cpp
@@ -4,6 +4,8 @@
 
 //clang++ -O3 -std=c++11 -I../.. -I/usr/local/include -I/Users/Betancourt/Documents/Research/Code/stan-dev/cmdstan/stan/lib/gtest_1.7.0/include -L/Users/Betancourt/Documents/Research/Code/stan-dev/cmdstan/stan/test -lgtest -o run_scalar_function_tests run_scalar_function_tests.cpp
 
+clang++ -O3 -std=c++11 -I/Users/Betancourt/Documents/Research/Code/stan-dev/nomad -I/usr/local/include -I/Users/Betancourt/Documents/Research/Code/stan-dev/cmdstan/stan/lib/gtest_1.7.0/include -L/Users/Betancourt/Documents/Research/Code/stan-dev/cmdstan/stan/test -lgtest -o acos_test acos_test.cpp /Users/Betancourt/Documents/Research/Code/stan-dev/cmdstan/stan/lib/gtest_1.7.0/src/gtest_main.cc
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
