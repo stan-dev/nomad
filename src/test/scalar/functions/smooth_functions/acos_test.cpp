c7afebb6900b018ae5f62346c0c0901499a96507
Michael Betancout <betanalpha@gmail.com>
2014-08-07 17:41:10 +0100

    Updated paths in example test

--- src/test/scalar/functions/smooth_functions/acos_test.cpp
+++ src/test/scalar/functions/smooth_functions/acos_test.cpp
@@ -3,9 +3,9 @@
 #include <math.h>
 #include <string>
 
-#include <autodiff/base_functor.hpp>
-#include <scalar/functions.hpp>
-#include <tests/finite_difference.hpp>
+#include <src/autodiff/base_functor.hpp>
+#include <src/scalar/functions.hpp>
+#include <src/test/finite_difference.hpp>
 
 // clang++ -O3 -std=c++11 -I/Users/Betancourt/Documents/Research/Code/stan-dev/nomad -I/usr/local/include -I/Users/Betancourt/Documents/Research/Code/stan-dev/cmdstan/stan/lib/gtest_1.7.0/include -L/Users/Betancourt/Documents/Research/Code/stan-dev/cmdstan/stan/test -lgtest -o acos_test acos_test.cpp /Users/Betancourt/Documents/Research/Code/stan-dev/cmdstan/stan/lib/gtest_1.7.0/src/gtest_main.cc
 
